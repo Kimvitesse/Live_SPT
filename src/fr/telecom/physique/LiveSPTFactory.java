5849499ffac2786ddecdbacc8caefd813044811e
FavreIndustries <bastien.favre.p@gmail.com>
2025-01-12 15:56:44 +0000

    Update LiveSPTFactory.java
    Ajout d'affichages pour débugger

--- src/fr/telecom/physique/LiveSPTFactory.java
+++ src/fr/telecom/physique/LiveSPTFactory.java
@@ -1,8 +1,8 @@
 // Projet LiveSPT
 // Auteurs : Yzouille, Kimvitesse, Sananas03, Poissondavril03, FavreIndustries
 // Date de création : 08/12/2024
-// Date de modification : 08/12/2024
-// Version : 0.1
+// Date de modification : 12/01/2025
+// Version : 0.2
 
 package fr.telecom.physique;
 
@@ -20,12 +20,14 @@ public class LiveSPTFactory implements ProcessorFactory {
     // Constructeur(s)
     public LiveSPTFactory(PropertyMap pm) {
         property_map = pm;
+        System.out.println("Usine appelée");
     }
 
 
     // Méthodes
     @Override
     public Processor createProcessor() {
+        System.out.println("Processeur créé");
         return new LiveSPTProcessor();
     }
 }
