03077bbc4e1e68d2b63fb7bf5453a6ef2d66dc8d
FavreIndustries <bastien.favre.p@gmail.com>
2025-01-17 15:10:54 +0100

    Update LiveSPTProcessor.java
    Added ALICA spots detector

--- src/fr/telecom/physique/LiveSPTProcessor.java
+++ src/fr/telecom/physique/LiveSPTProcessor.java
@@ -6,17 +6,22 @@
 
 package fr.telecom.physique;
 
+import java.awt.Polygon;
+
 import org.micromanager.data.Processor;
 import org.micromanager.data.Image;
 import org.micromanager.data.ProcessorContext;
 
-import fiji.plugin.trackmate.SpotCollection;
+import ch.epfl.leb.alica.acpack.analyzers.spotcounter.FindLocalMaxima;
+
+import ij.process.ShortProcessor;
+import ij.gui.Roi;
 
 
 // Cette classe implémente le traitement d'image
 public class LiveSPTProcessor implements Processor {
     // Attributs
-    private SpotCollection spots // attribut qui permet de stocker les spots des images
+    private Polygon spots // attribut qui permet de stocker les spots des images
     
 
 
@@ -33,7 +38,12 @@ public class LiveSPTProcessor implements Processor {
     public void processImage(Image image, ProcessorContext context) {
         // Détection de points
         System.out.println("Méthode processImage appelée");
-
+        ShortProcessor img = new ShortProcessor(512, 512);
+        Roi roi = new Roi(0, 0, 512, 512);
+        img.setPixels(image.getRawPixels());
+        spots = FindLocalMaxima.FindMax(img, roi, 0, 0, FindLocalMaxima.FilterType.NONE);
+        System.out.println(spots.xpoints.toString());
+        System.out.println(spots.ypoints.toString());
 
         context.outputImage(image); // rend l'image pour les processeurs suivants
     }
