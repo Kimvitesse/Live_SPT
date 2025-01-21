64f3e1645c4551d9979f9bbef1c16fd1bfce9926
FavreIndustries <bastien.favre.p@gmail.com>
2025-01-21 16:22:46 +0000

    Update LiveSPTFactory.java
    Ajout d'un attribut Studio

--- src/fr/telecom/physique/LiveSPTFactory.java
+++ src/fr/telecom/physique/LiveSPTFactory.java
@@ -1,25 +1,29 @@
 // Projet LiveSPT
 // Auteurs : Yzouille, Kimvitesse, Sananas03, Poissondavril03, FavreIndustries
 // Date de création : 08/12/2024
-// Date de modification : 12/01/2025
-// Version : 0.2
+// Date de modification : 21/01/2025
+// Version : 0.3
 
 package fr.telecom.physique;
 
 import org.micromanager.data.ProcessorFactory;
 import org.micromanager.data.Processor;
 import org.micromanager.PropertyMap;
+import org.micromanager.Studio;
 
 
 // J'ai pas trop compris à quoi elle sert celle là mais elle doit être là
 public class LiveSPTFactory implements ProcessorFactory {
     // Attributs
     private PropertyMap property_map;
+    private Studio app;
 
 
     // Constructeur(s)
-    public LiveSPTFactory(PropertyMap pm) {
+    public LiveSPTFactory(PropertyMap pm, Studio app) {
+        super();
         property_map = pm;
+        this.app = app;
         System.out.println("Usine appelée");
     }
 
@@ -28,6 +32,6 @@ public class LiveSPTFactory implements ProcessorFactory {
     @Override
     public Processor createProcessor() {
         System.out.println("Processeur créé");
-        return new LiveSPTProcessor();
+        return new LiveSPTProcessor(property_map, app);
     }
 }
