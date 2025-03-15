d4a14894bf2fdb05df0e32feeb63792c97dc8e62
FavreIndustries <bastien.favre.p@gmail.com>
2025-03-15 15:07:49 +0000

    Update ConfigWindow.java

--- src/fr/telecom/physique/ConfigWindow.java
+++ src/fr/telecom/physique/ConfigWindow.java
@@ -1,8 +1,8 @@
 // Projet LiveSPT
 // Auteurs : Yzouille, Kimvitesse, Sananas03, Poissondavril03, FavreIndustries
 // Date de création : 10/02/2025
-// Date de modification : 10/02/2025
-// Version : 0.3
+// Date de modification : 15/03/2025
+// Version : 0.7
 
 package fr.telecom.physique;
 
@@ -10,40 +10,104 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import org.micromanager.PropertyMap;
+import org.micromanager.Studio;
 
 public class ConfigWindow extends JDialog {
     private JTextField roiField;
     private JTextField minDistanceField;
-    private JTextField thresholdField;
+    private JTextField detectionThresholdField;
+    private JTextField reducedRoiField;
+    private JTextField initialRoiXField;
+    private JTextField initialRoiYField;
+    private JTextField ellipseThresholdField;
+    private JTextField aField;
+    private JTextField bField;
+    private JTextField dField;
+    private JTextField cxField;
+    private JTextField cyField;
+    private JTextField w0Field;
+    private JTextField realSizeField;
+    private JTextField stageNameField;
+    private Studio app;
     private PropertyMap config;
 
     public ConfigWindow(JFrame parent, PropertyMap conf) {
         super(parent, true);
         config = conf;
         setTitle("Configuration");
-        setSize(550, 200);
+        setSize(550, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new GridLayout(4, 2));
+        setLayout(new GridLayout(16, 2));
 
         add(new JLabel("Taille ROI (pix) :"));
         roiField = new JTextField();
         add(roiField);
 
+        add(new JLabel("Coordonnée initiale en x de la ROI (pix) :"));
+        initialRoiXField = new JTextField();
+        add(initialRoiXField);
+
+        add(new JLabel("Coordonnée initiale en y de la ROI (pix) :"));
+        initialRoiYField = new JTextField();
+        add(initialRoiYField);
+
+        add(new JLabel("Taille de la ROI réduite (pix) :"));
+        reducedRoiField = new JTextField();
+        add(reducedRoiField);
+
         add(new JLabel("Distance min entre particules (pix) :"));
         minDistanceField = new JTextField();
         add(minDistanceField);
 
-        add(new JLabel("Seuil de luminosité (/65 535) :"));
-        thresholdField = new JTextField();
-        add(thresholdField);
+        add(new JLabel("Seuil de luminosité pour la détection (/65 535) :"));
+        detectionThresholdField = new JTextField();
+        add(detectionThresholdField);
 
-        JButton validateButton = new JButton("Valider");
+        add(new JLabel("Seuil de luminosité pour l'ellipse (/65 535) :"));
+        ellipseThresholdField = new JTextField();
+        add(ellipseThresholdField);
+
+        add(new JLabel("Coefficient de calibration A :"));
+        aField = new JTextField();
+        add(aField);
+
+        add(new JLabel("Coefficient de calibration B :"));
+        bField = new JTextField();
+        add(bField);
+
+        add(new JLabel("Coefficient de calibration d :"));
+        dField = new JTextField();
+        add(dField);
+
+        add(new JLabel("Coefficient de calibration w0 :"));
+        w0Field = new JTextField();
+        add(w0Field);
+
+        add(new JLabel("Coefficient de calibration cx :"));
+        cxField = new JTextField();
+        add(cxField);
+
+        add(new JLabel("Coefficient de calibration cy :"));
+        cyField = new JTextField();
+        add(cyField);
+
+        add(new JLabel("Taille réelle de l'échantillon (µm) :"));
+        realSizeField = new JTextField();
+        add(realSizeField);
+
+        add(new JLabel("Nom de la platine piezoélectrique :"));
+        stageNameField = new JTextField();
+        add(stageNameField);
+
+        JButton validateButton = new JButton("OK");
         add(validateButton);
 
         validateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                checkSettings();
                 saveConfig();
                 dispose(); // Ferme la fenêtre après validation
             }
@@ -52,15 +116,57 @@ public class ConfigWindow extends JDialog {
         setLocationRelativeTo(parent); // Centre la fenêtre de réglage
     }
 
+    /**
+     * Checks whether the inputted settings are in the correct format
+     */
+    private void checkSettings() {
+        try
+        {
+            Integer.parseInt(roiField.getText());
+            Integer.parseInt(reducedRoiField.getText());
+            Integer.parseInt(initialRoiXField.getText());
+            Integer.parseInt(initialRoiYField.getText());
+            Integer.parseInt(minDistanceField.getText());
+            Integer.parseInt(detectionThresholdField.getText());
+            Integer.parseInt(ellipseThresholdField.getText());
+            Double.parseDouble(aField.getText());
+            Double.parseDouble(bField.getText());
+            Double.parseDouble(dField.getText());
+            Double.parseDouble(w0Field.getText());
+            Double.parseDouble(cxField.getText());
+            Double.parseDouble(cyField.getText());
+            Integer.parseInt(realSizeField.getText());
+        }
+        catch (NumberFormatException e)
+        {
+            app.logs().showError("Wrong input format.");
+        }
+    }
+
+    /**
+     * Updates the PropertyMap with the new settings
+     */
     private void saveConfig() {
         try {
             config = config.copyBuilder()
                 .putInteger("RoiSize", Integer.parseInt(roiField.getText()))
+                .putInteger("ReducedRoiSize", Integer.parseInt(reducedRoiField.getText()))
+                .putInteger("InitialRoiX", Integer.parseInt(initialRoiXField.getText()))
+                .putInteger("InitialRoiY", Integer.parseInt(initialRoiYField.getText()))
                 .putInteger("MinDistToOtherMax", Integer.parseInt(minDistanceField.getText()))
-                .putInteger("Threshold", Integer.parseInt(thresholdField.getText()))
+                .putInteger("DetectionThreshold", Integer.parseInt(detectionThresholdField.getText()))
+                .putInteger("EllipseThreshold", Integer.parseInt(ellipseThresholdField.getText()))
+                .putDouble("A", Double.parseDouble(aField.getText()))
+                .putDouble("B", Double.parseDouble(bField.getText()))
+                .putDouble("d", Double.parseDouble(dField.getText()))
+                .putDouble("w0", Double.parseDouble(w0Field.getText()))
+                .putDouble("cx", Double.parseDouble(cxField.getText()))
+                .putDouble("cy", Double.parseDouble(cyField.getText()))
+                .putInteger("RealSize", Integer.parseInt(realSizeField.getText()))
+                .putString("StageName", stageNameField.getText())
                 .build();
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(this, "Veuillez entrer des entiers", "Erreur", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Veuillez entrer des valeurs", "Erreur", JOptionPane.ERROR_MESSAGE);
         }
         System.out.println("Taille de la map config : " + config.size());
     }
