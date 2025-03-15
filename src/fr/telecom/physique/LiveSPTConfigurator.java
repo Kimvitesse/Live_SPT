0dc2f780da26bc5a95a585e4984160236cea7f8d
FavreIndustries <bastien.favre.p@gmail.com>
2025-03-15 15:09:13 +0000

    Update LiveSPTConfigurator.java

--- src/fr/telecom/physique/LiveSPTConfigurator.java
+++ src/fr/telecom/physique/LiveSPTConfigurator.java
@@ -1,7 +1,7 @@
 // Projet LiveSPT
 // Auteurs : Yzouille, Kimvitesse, Sananas03, Poissondavril03, FavreIndustries
 // Date de création : 08/12/2024
-// Date de modification : 10/02/2025
+// Date de modification : 15/03/2025
 // Version : 0.4
 
 package fr.telecom.physique;
@@ -12,21 +12,20 @@ import org.micromanager.PropertyMap;
 
 // Cette classe ouvre une fenêtre graphique pour que l'utilisateur règle le plugin
 // Elle renvoie les paramètres dans le PropertyMap à destination du ProcessorFactory
+/**
+ * Configures the plugin by opening a GUI and returning the settings to the Factory
+ */
 public class LiveSPTConfigurator implements ProcessorConfigurator {
     // Attributs
     public PropertyMap property_map;
-    private ConfigWindow frame; // static ?
+    private ConfigWindow frame;
 
     
-    // Constructeur(s)
+    // Constructeur
     public LiveSPTConfigurator(PropertyMap pm) {
         // Ce constructeur est appelé à chaque fois qu'une instance du plugin est ajouté à la pipeline
         property_map = pm;
-        System.out.println("Configurateur appelé");
         frame = new ConfigWindow(null, property_map);
-        // attendre que la fenêtre soit fermée et récupérer les valeurs
-        property_map = frame.getConfig();
-        System.out.println("Taille de la map Configurator : " + property_map.size());
     }
 
 
@@ -39,6 +38,7 @@ public class LiveSPTConfigurator implements ProcessorConfigurator {
     @Override
     public void showGUI() {
         frame.setVisible(true);
+        property_map = frame.getConfig();
         return;
     }
 
